9c86f143084ad10e59e537c3ff9d8d629b052df2
A1RG3MM <cr3ative.ee@gmail.com>
2024-02-03 17:51:42 -0600

    more stuff ig

---
+++ public/js/clock.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Your code here
+  function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+  
+    if (h == 0) {
+      h = 12;
+    }
+  
+    if (h > 12) {
+      h = h - 12;
+      session = "PM";
+    }
+  
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+  
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("timer").innerText = time;
+    document.getElementById("timer").textContent = time;
+  
+    setTimeout(showTime, 1000);
+  }
+  
+  showTime();
+});
