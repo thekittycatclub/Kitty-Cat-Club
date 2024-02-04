d038a70312d5c5d6b5a4fe1ac52091b35df8501c
A1RG3MM <cr3ative.ee@gmail.com>
2024-02-03 18:08:31 -0600

    Co-authored-by: Mecharis <mecharis-420@users.noreply.github.com>

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:19-bullseye
+ENV NODE_ENV=production
+WORKDIR /app
+COPY ["package.json", "package-lock.json*", "./"]
+RUN npm install
+EXPOSE 8080
+COPY . .
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const server = http.createServer();
 const bareServer = createBareServer("/bare/");
 
 app.use(express.static(path.join(__dirname, "public")));
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
